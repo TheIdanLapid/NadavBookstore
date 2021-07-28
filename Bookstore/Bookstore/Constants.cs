4d608559d443f06673b3bef65826301f37f889d9
TheIdanLapid <eontorch@gmail.com>
2021-07-28 22:59:37 +0300

    CSV Helper doesn't write

---
+++ Bookstore/Bookstore/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookstore
+{
+    public class Constants
+    {
+        internal static readonly string filename = "books.json";
+        internal static readonly string outputType = "csv";
+    }
+}
